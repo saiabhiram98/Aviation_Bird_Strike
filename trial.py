8fb210a8fe54415fd7b0acd4a8d92bf4ef584be4
Sai Abhiram Addanki <asaiabhiram@gmail.com>
2023-12-10 01:08:39 -0500

    added pages

--- trial.py
+++ trial.py
@@ -10,38 +10,44 @@ def trialrun():
     return df2
    
 df2 = trialrun()
+st.title("Airplane bird strikes analysis in the US")
+page = st.sidebar.selectbox("Select a page ",["Select","Filtered by Phase of Flight", "Filtered by Airports"])
 
-selected_option = st.selectbox("Select an option: ", df2['New Phase of Flight'].unique())
+def phase_flight(df2):
+    selected_option = st.sidebar.selectbox("Select the phase of flight you want to get some graphs of: ", df2['New Phase of Flight'].unique())
 
-data = df2[df2["New Phase of Flight"] == selected_option]
+    data = df2[df2["New Phase of Flight"] == selected_option]
 
-counts = data.groupby('Strike part').size().reset_index(name='count')
-counts['percentage'] = ((counts['count'] / counts['count'].sum()) * 100).round(2)
+    counts = data.groupby('Strike part').size().reset_index(name='count')
+    counts['percentage'] = ((counts['count'] / counts['count'].sum()) * 100).round(2)
 
-gradient_to = "#ed0000"
-gradient_from = "#fcc5c5"
+    gradient_to = "#ed0000"
+    gradient_from = "#fcc5c5"
 
-alt_chart = (alt.Chart(counts , title = f"Bird strike percentgae on airplane part during {selected_option}").mark_bar()
-             .encode(x = 'Strike part:N',
-                     y = 'percentage:Q',
-                     color=alt.Color('percentage:Q', scale=alt.Scale(range=[gradient_from, gradient_to]))
-                     )
-                     .interactive())
+    alt_chart = (alt.Chart(counts , title = f"Bird strike percentage on airplane part during {selected_option}").mark_bar()
+                .encode(x = 'Strike part:N',
+                        y = 'percentage:Q',
+                        color=alt.Color('percentage:Q', scale=alt.Scale(range=[gradient_from, gradient_to]))
+                        )
+                        .interactive())
 
-st.altair_chart(alt_chart, use_container_width= True)
+    st.altair_chart(alt_chart, use_container_width= True)
 
-counts2 = data.groupby('Airport').size().reset_index(name='count')
-counts2 = counts2.sort_values(by = 'count', ascending = False)
-counts2 = counts2.head(10)
+    counts2 = data.groupby('Airport').size().reset_index(name='count')
+    counts2 = counts2.sort_values(by = 'count', ascending = False)
+    counts2 = counts2.head(10)
 
-gradient_to = "#ed0000"
-gradient_from = "#fcc5c5"
+    gradient_to = "#ed0000"
+    gradient_from = "#fcc5c5"
 
-alt_chart2 = (alt.Chart(counts2 , title = f"Bird strikes on airport during {selected_option}").mark_bar()
-             .encode(x = 'Airport:N',
-                     y = 'count:Q',
-                     color=alt.Color('count:Q', scale=alt.Scale(range=[gradient_from, gradient_to]))
-                     )
-                     .interactive())
+    alt_chart2 = (alt.Chart(counts2 , title = f"Bird strikes in top 10 airports during {selected_option}").mark_bar()
+                .encode(x = 'Airport:N',
+                        y = 'count:Q',
+                        color=alt.Color('count:Q', scale=alt.Scale(range=[gradient_from, gradient_to]))
+                        )
+                        .interactive())
 
-st.altair_chart(alt_chart2, use_container_width= True)
+    st.altair_chart(alt_chart2, use_container_width= True)
+
+if page == "Filtered by Phase of Flight":
+    phase_flight(df2)
